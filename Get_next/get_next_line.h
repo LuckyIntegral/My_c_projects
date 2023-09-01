1e6465f61757698464468eb1d1b12e5cd213b018
Vitalik <frantsv2004@gmail.com>
2023-09-01 14:36:53 +0200

    add basic structure

---
+++ Get_next/get_next_line.h
@@ -0,0 +1,21 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   get_next_line.h                                    :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: vfrants <frantsv2004@gmail.com>            +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/08/31 23:32:02 by vfrants           #+#    #+#             */
+/*   Updated: 2023/08/31 23:49:06 by vfrants          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef GET_NEXT_LINE
+# define GET_NEXT_LINE
+
+# include <stdlib.h>
+# include <unistd.h>
+
+int get_next_line(int fd, char **line);
+
+#endif
